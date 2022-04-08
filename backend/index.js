23596bf53d464b2688ad06de4f7e5d3814a553fb
IT20228576 <IT20228576@gmail.com>
2022-04-08 21:56:46 +0530

    connect mongodb

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// set up server
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Successfully Server started on : ${PORT}`));
+
+// connect to mongoDB
+
+mongoose.connect(
+  process.env.DBLINK,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Successfully Connected to MongoDB");
+  }
+);

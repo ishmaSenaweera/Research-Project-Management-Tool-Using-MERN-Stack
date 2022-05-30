1c90ff760b9f97f651fb24bdfdd3d77f03bd93bd
IT20228576 <IT20228576@gmail.com>
2022-05-30 13:09:52 +0530

    change logout button

--- frontend/src/components/userManagement/auth/Logout.jsx
+++ frontend/src/components/userManagement/auth/Logout.jsx
@@ -19,7 +19,7 @@ function LogOut() {
   }
 
   return (
-    <button className="btn btn-danger account-button" onClick={logOut}>
+    <button class="btn btn-outline-light" onClick={logOut}>
       Log out
     </button>
   );

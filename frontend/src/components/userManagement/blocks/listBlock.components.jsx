c5b5601b63e35d84da546cdcd7ba7f0822ea13cf
IT20228576 <IT20228576@gmail.com>
2022-05-31 17:46:36 +0530

    add search funtion in the list block component

--- frontend/src/components/userManagement/blocks/listBlock.components.jsx
+++ frontend/src/components/userManagement/blocks/listBlock.components.jsx
@@ -1,37 +1,48 @@
+import { useState } from "react";
 import Table from "react-bootstrap/esm/Table";
 
 const BlockList = (props) => {
   const { data } = props;
+  const [search, setSearch] = useState("");
 
   function customerList() {
     return data.map((current, index) => {
-      return (
-        <tr key={index}>
-          <td>{index + 1}</td>
-          <td>{current.name}</td>
-          <td>{current.email}</td>
-          {props.heading === "Students" ? (
-            <td>{current.sid}</td>
-          ) : (
-            <td>{current.nic}</td>
-          )}
+      const name = current.name;
+      if (name.toLowerCase().includes(search.toLowerCase()) || search === "") {
+        return (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{current.name}</td>
+            <td>{current.email}</td>
+            {props.heading === "Students" ? (
+              <td>{current.sid}</td>
+            ) : (
+              <td>{current.nic}</td>
+            )}
 
-          <td>
-            <button
-              className="btn btn-primary account-button-blue"
-              onClick={props.viewDetails.bind(this, current)}
-            >
-              View
-            </button>
-          </td>
-        </tr>
-      );
+            <td>
+              <button
+                className="btn btn-primary account-button-blue"
+                onClick={props.viewDetails.bind(this, current)}
+              >
+                View
+              </button>
+            </td>
+          </tr>
+        );
+      }
     });
   }
 
   return (
     <div className="list">
       <div className="list-sub-table">
+          <input
+            type="text"
+            placeholder="Search"
+            className="search-input"
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
         <div className="head">
           <h1>{props.heading}</h1>
           {props.heading !== "Students" && (

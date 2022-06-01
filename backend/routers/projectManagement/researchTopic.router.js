6befe58163e512e8000efdec695d4ca07d5ae636
ishmaSenaweera <86140992+ishmaSenaweera@users.noreply.github.com>
2022-06-01 18:41:59 +0530

    Enhancements

--- backend/routers/projectManagement/researchTopic.router.js
+++ backend/routers/projectManagement/researchTopic.router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const {
-  saveResearchTopic,
+  saveResearchTopic, getAllResearchTopic, updateResearchTopic, deleteResearchTopic
 } = require("../../controllers/projectManagement/researchTopic.controller");
 const researchTopicRouter = express.Router();
 
 researchTopicRouter.post("/save", saveResearchTopic);
+researchTopicRouter.get("/", getAllResearchTopic);
+researchTopicRouter.put("/update/:id", updateResearchTopic);
+researchTopicRouter.delete("/delete/:id", deleteResearchTopic);
 
 module.exports = researchTopicRouter;

54471f02d593a76ed893c6c4b7f3a58ba1a25ae4
ishmaSenaweera <86140992+ishmaSenaweera@users.noreply.github.com>
2022-06-04 02:56:47 +0530

    Bug fixes

--- frontend/src/components/projectManagement/templates/fileShowScreen.jsx
+++ frontend/src/components/projectManagement/templates/fileShowScreen.jsx
@@ -6,14 +6,15 @@ import { useLocation } from "react-router-dom";
 function FileShowScreen() {
   const [componentType, setComponentType] = useState("1")
   const { state } = useLocation();
+  const [activeTab, setActiveTab] = useState(false);
 
   return (
     <div className="container-md p-10 mt-5">
       <div className="card">
         <div className="card-header">
           <ul class="nav nav-tabs">
-            <li class="nav-item"><span className="nav-link" onClick={()=> setComponentType("1")}>Templates</span></li>
-            <li class="nav-item"><span className="nav-link" onClick={()=> setComponentType("2")}>Other Templates</span></li>
+            <li class="nav-item"><button className="btn" onClick={()=> {setComponentType("1"), setActiveTab(true)}}>Templates</button></li>
+            <li class="nav-item"><button className="btn" onClick={()=> {setComponentType("2"), setActiveTab(true)}}>Other Templates</button></li>
           </ul>
         </div>
         <div className="card-body">

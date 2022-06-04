1bfc6cf6995005765475847e35b3ccbb787d8c7a
IT20228576 <IT20228576@gmail.com>
2022-06-04 14:40:16 +0530

    update home.css

--- frontend/src/components/userManagement/Home.jsx
+++ frontend/src/components/userManagement/Home.jsx
@@ -1,10 +1,11 @@
 import homeImage from "../../../images/home.jpeg";
+import "./home.css";
 
 function Home() {
   return (
     <div className="container mb-5">
       <h1 className="home-title-style">Research Management Tool</h1>
-      <img src={homeImage} className="home-image-style"/>
+      <img src={homeImage} className="home-image-style" />
     </div>
   );
 }
